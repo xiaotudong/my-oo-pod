a2b2ffeabb547563addc93ef35d6d9b4671e0403
dingqian <302965061@qq.com>
2016-07-26 01:00:28 +0800

    move buildCartItems funtion to CartItem.buildCartItems method

--- src/models/cart-item.js
+++ src/models/cart-item.js
@@ -1,8 +1,30 @@
- class CartItem {
-   constructor(item,count = 1){
-     this.item = item;
-     this.count = count;
-   }
- }
+class CartItem {
+  constructor(item, count = 1) {
+    this.item = item;
+    this.count = count;
+  }
 
- module.exports = CartItem;
+  static buildCartItems(tags, allItems) {
+
+    const cartItems = [];
+    for (const tag of tags) {
+
+      const tagArray = tag.split('-');
+      const barcode = tagArray[0];
+      const count = parseFloat(tagArray[1] || 1);
+
+      const cartItem = cartItems.find(cartItem => cartItem.item.barcode === barcode);
+
+      if (cartItem) {
+        cartItem.count += count;
+      } else {
+        const item = allItems.find(item => item.barcode === barcode);
+        cartItems.push(new CartItem(item, count));
+      }
+    }
+
+    return cartItems;
+  }
+}
+
+module.exports = CartItem;
